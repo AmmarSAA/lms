ef2f145250d44293fe5c98f4dbb648ae63a9b7ff
Ammar S.A.A <s.ammarahmed14@gmail.com>
2022-04-07 18:27:59 +0500

    minor big fix

--- slide_add.php
+++ slide_add.php
@@ -17,16 +17,17 @@ include(WEBSITE_PATH.'./includes/menu.php');
 if (isset($_POST['slide-add']))
 {
 	
-	$slide_no 			= $_POST['slide_no'];
+	$slide_no 			= 100;
 	$title 				= trim($_POST['title']);
 	$content 			= trim($_POST['content']);
+	$slider_img_checker = trim($_POST['slider_img_checker']);
 	$slider_img 		= $_FILES['slider_img']['name'];
 	$slider_img_temp 	= $_FILES['slider_img']['tmp_name'];
 
 	//Moves uploaded SLider Image to a permenent location
 	move_uploaded_file($slider_img_temp,"./images/slider/$slider_img");
 	
-	if (!empty($title)){
+	if (!empty($slider_img_checker) ){
 		if (isset($_GET['id']))
 		{
 			if(empty($slider_img)) 
@@ -40,18 +41,17 @@ if (isset($_POST['slide-add']))
 				}
 
 			}
-			$sql = "UPDATE tblslider SET
-			`slide_no` 		= '{$slide_no}', 
+			$sql = "UPDATE tblslider SET 
         	`title` 		= '{$title}',
         	`content` 		= '{$content}',
 			`slider_img` 	= '{$slider_img}'
         	WHERE id = {$_POST['id']}";
 		}
 		else{
-			$sql = "INSERT INTO `tblslider`(`slide_no`,`title`, `content`, `slider_img`) 
-				VALUES ({slide_no}, '{$title}', '{$content}', '{$slider_img}')";
+			$sql = "INSERT INTO `tblslider`(`title`, `content`, `slider_img`) 
+				VALUES ('{$title}', '{$content}', '{$slider_img}')";
 		}
-		if ($slide_no < 1) {
+		if ($slide_no < 0) {
 			$msg = "<div class='alert alert-info text-capitalize'><strong>slide no.</strong> can never be <strong>0 or lower</strong>.</div>";
 		}elseif (IfExist(TBLSLIDER, 'slide_no', $slide_no)) {
 			$msg = "<div class='alert alert-warning text-capitalize'><strong>slide no. ".$slide_no."</strong> already exists, To View the list Click/Tap <a href='".WEBSITE_URL."/slide_list.php'>HERE</a>.</div>";
@@ -65,14 +65,13 @@ if (isset($_POST['slide-add']))
 			}
 		}
 	}else{
-		$msg = "<div class='alert alert-danger text-capitalize'>topic or/and slide no. can never be empty.</div>";
+		$msg = "<div class='alert alert-danger text-capitalize'>slide picture can never be empty.</div>";
 	}
 }
 
 $title 			= '';
 $content 		= '';
 $slider_img 	= '';
-$slide_no 		= '';
 
 if (isset($_GET['id']))
 {
@@ -81,7 +80,6 @@ $result = $conn->query($select);
 	if ($result && $result->num_rows > 0){
 		$row = $result->fetch_assoc();
 		$id 			= $row['id'];
-		$slide_no 			= $row['slide_no'];
 		$title 			= $row['title'];
 		$content 		= $row['content'];
 		$slider_img 	= $row['slider_img'];
@@ -92,52 +90,52 @@ if (isset($msg))
 {
 	echo $msg;
 }
-?>
-      							
-			<section id="content">
-				<div class="page-wrapper">
-					<div class="container-fluid">
-						<div class="row">
-							<div class="col">
-								<!--SlideAdd Form-->
-								<form class="" name="slide-add" method="post" action="#" enctype="multipart/form-data">
-									<br />
-									<input type="hidden" name="slide-add" value="slide-add" />
-									<input type="hidden" name="id" value="<?php echo $id;?>" />
-									<span class="f-img fa fa-slideshare fa-4x"></span>
-									<h2>Slide</h2>
-									<p>Add Slide Form</p>
-									<p class="labelenglish"><b>Picture:</b></p>
-									<input type="file" accept="img/*" value="<?php echo $slider_img ?>" name="slider_img" class="labelenglish text-uppercase" />
-									<p class="labelenglish"><small><b>Note:</b><br /> Your <b class="text-uppercase text-right"><?php if(empty($slider_img)){ echo 'Picture'; }else{ echo $slider_img; } ?></b> must not be more than <b>11 MB</b>.</small></p>
-									<p class="labelenglish"><b>Slide No.:</b></p>
-									<input type="number" value="<?php echo $slide_no ?>" name="slide_no" class="blank" required />
-									<p class="labelenglish"><b>Topic:</b></p>
-									<input type="text" value="<?php echo $title ?>" name="title" class="blank" required />
-									<p class="labelenglish"><b>Content:</b></p>
-									<textarea  class="blank" name="content"><?php echo $content ?></textarea>
-									<div>
-										<input type="reset" name="reset" value="Reset"  class="btn btn-success"/>
-										<?php 
+?> 							
+		<section id="content">
+			<div class="page-wrapper">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col">
+							<!--SlideAdd Form-->
+							<form class="" name="slide-add" method="post" action="#" enctype="multipart/form-data">
+								<br />
+								<input type="hidden" name="slide-add" value="slide-add" />
+								<input type="hidden" name="id" value="<?php echo $id;?>" />
+								<span class="f-img fa fa-slideshare fa-4x"></span>
+								<h2>Slide</h2>
+								<p>Add Slide Form</p>
+								<p class="labelenglish"><b>Picture:</b></p>
+								<input type="file" accept="img/*" value="<?php echo $slider_img ?>" name="slider_img" class="labelenglish text-uppercase"/>
+								<input type="hidden" name="slider_img_checker" value="<?php echo $slider_img;?>" />
+								<p class="labelenglish"><small><b>Note:</b><br /> Your <b class="text-uppercase text-right"><?php if(empty($slider_img)){ echo 'Picture'; }else{ echo $slider_img; } ?></b> must not be more than <b>11 MB</b>.</small></p>
+								<!--<p class="labelenglish"><b>Slide No.:</b></p>
+								<input type="number" value="<?php //echo $slide_no ?>" name="slide_no" class="blank" required hidden/>-->
+								<p class="labelenglish"><b>Topic:</b></p>
+								<input type="text" value="<?php echo $title ?>" name="title" class="blank" />
+								<p class="labelenglish"><b>Content:</b></p>
+								<textarea  class="blank" name="content"><?php echo $content ?></textarea>
+								<div>
+									<input type="reset" name="reset" value="Reset"  class="btn btn-success"/>
+									<?php 
 
-										if (isset($_GET['id'])) {
-											echo "<input type='submit' name='submit' value='Update'  class='btn btn-success' />";
-										}
-										else{
-											echo "<input type='submit' name='submit' value='Add'  class='btn btn-success' />";
-										}
+									if (isset($_GET['id'])) {
+										echo "<input type='submit' name='submit' value='Update'  class='btn btn-success' />";
+									}
+									else{
+										echo "<input type='submit' name='submit' value='Add'  class='btn btn-success' />";
+									}
 
-										?>
-									</div>
-									<br />
-								</form>
-							</div>
+									?>
+								</div>
+								<br />
+							</form>
 						</div>
 					</div>
 				</div>
-			</section>
-		</div>
+			</div>
+		</section>
 	</div>
+</div>
 <?php
 include(WEBSITE_PATH.'./includes/footer.php');
 ?>
